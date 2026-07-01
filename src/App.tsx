b08ad5cabde6197538986032117740e6604162c5
agatamarina <116568387+agatamarina@users.noreply.github.com>
2026-07-01 12:46:26 +0000

    rodape

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Minha Empresa</p>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">LinkedIn</a></li>
+      </ul>
+    </footer>
+  );
+}
+     
